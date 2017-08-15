58063e265347fd5cdab8235650d6761b8939e17a
vinicius~~ <viniciusferreirwk@gmail.com>
2017-08-14 22:04:58 -0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,12 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
+.config(function($stateProvider, $urlRouterProvider,$sceDelegateProvider) {
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    '*://*:*/**',
+    'http://*:*/**',
+  ]);
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,56 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope,$timeout) {})
 
-.controller('ChatsCtrl', function($scope, Chats,$timeout) {
+.controller('ChatsCtrl', function($scope, Chats,$timeout,$http) {
+  $scope.contaNOIP         = "viniciusfs:995865Aa@";
+  $scope.dnsNOIP           = "testesmart.ddns.net";
+  $scope.atualizarDNS      = {estado:false,availableOptions: [
+      {id: true, name: 'SIM'},
+      {id: false, name: 'NÃO'},
+      
+  ]};
+  $scope.atualizarDNSTempo = 5000;//ms
 
+  $scope.atualizaDNS = function(){
+    if($scope.atualizarDNS.estado){
+      $http({
+        method: 'GET',
+        url: 'http://ipv4.myexternalip.com/json'
+      }).then(function successCallback(response) {
+        console.log(response.data.ip);
+        $http({
+          method: 'GET',
+          url: 'http://'+$scope.contaNOIP+'dynupdate.no-ip.com/nic/update?hostname='+$scope.dnsNOIP+'&myip='+response.data.ip
+        }).then(function successCallback(response) {
+            console.log(response.data);
+
+            $timeout(function(){
+              $scope.atualizaDNS();
+            },$scope.atualizarDNSTempo);
+            
+          }, function errorCallback(response) {
+            
+            $timeout(function(){
+              $scope.atualizaDNS();
+            },$scope.atualizarDNSTempo);
+          
+          });
+
+        }, function errorCallback(response) {
+          $timeout(function(){
+            $scope.atualizaDNS();
+          },$scope.atualizarDNSTempo);
+          
+        });
+    }else{
+      $timeout(function(){
+        $scope.atualizaDNS();
+      },$scope.atualizarDNSTempo);
+    }
+  }
+  $scope.atualizaDNS();
+  //CHECAR SE JA EXISTE A WINDOW.HTTPD.REQUESTS NO INIT QUANDO TIVER EM BACKGROUND
   $scope.server = {};
   $scope.server.porta = 10000;
   $scope.server.senha = "teste";
