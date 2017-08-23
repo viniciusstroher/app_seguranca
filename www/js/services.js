584c9fbfebb5c26a2efb7b59741dbc958c9e7fa2
viniciusstroher <vinicius.stroher@outlook.com.br>
2017-08-23 13:24:41 -0300

    Update services.js

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,26 @@
 angular.module('starter.services', [])
-
+.factory('localFactory', function() {
+    var pacote = "br.com.tcc_ads.view.";
+    return {
+        set: function(name, data) {
+            localStorage.setItem(pacote + name, JSON.stringify(data));
+            return true;
+        },
+        get: function(name) {
+            try{
+                return JSON.parse(localStorage.getItem(pacote + name));
+            }catch(Exception){
+                return false;
+            }
+        },
+        delete: function(name) {
+            return localStorage.removeItem(pacote + name);
+        },
+        clear: function() {
+            return localStorage.clear();
+        }
+    };
+})
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
