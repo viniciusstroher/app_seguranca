81b2d7a305fb6540c573e62e148099ee4ecabe66
vini <vini@gmail.com>
2017-09-03 21:26:42 -0300

    fix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -27,32 +27,140 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('CamerasCtrl', function($scope,localFactory) {
+.controller('CamerasCtrl', function($scope,localFactory, $ionicPopup) {
   $scope.novaCamera           = {};
-  $scope.novaCamera.label     = "";
-  $scope.novaCamera.rtsp      = "";
-  $scope.novaCamera.descricao = "";
   
-  $scope.cameras    = [];
-  $scope.adicionar  = false;
-
-  var camerasCache  = localFactory.get('cameras'); 
-  if(camerasCache){
-    if(angular.isArray(camerasCache)){
-      $scope.cameras = camerasCache;
+  $scope.carrageCameras = function(){
+    $scope.cameras    = [];
+    var camerasCache  = localFactory.get('cameras'); 
+    if(camerasCache){
+      if(angular.isArray(camerasCache)){
+        $scope.cameras = camerasCache;
+      }
     }
   }
+  $scope.carrageCameras();
 
+  $scope.adicionar  = false;
+  $scope.editar     = false;
+  
   $scope.adicionarCamera = function function_name(argument) {
-    $scope.adicionar = true;
+    if(!$scope.editar){
+      $scope.adicionar = true;
+    }else{
+       $ionicPopup.show({
+          template: "Termine a edição antes de adicionar nova camera.",
+          title: 'Atenção',
+          
+          scope: $scope,
+          buttons: [
+           { text: 'Ok' },
+           
+          ]
+        });
+    }
+    
   }
 
   $scope.cancelarCamera = function(){
     $scope.adicionar = false;
+    $scope.editar    = false;
+    $scope.novaCamera       = {};
+ 
   }
 
   $scope.salvarCamera = function(){
     console.log($scope.novaCamera);
+    if($scope.novaCamera.label == "" || $scope.novaCamera.rtsp == ""){
+      $ionicPopup.show({
+        template: "Não deixe o campo de label ou o rtsp vazios.",
+        title: 'Atenção',
+        
+        scope: $scope,
+        buttons: [
+         { text: 'Ok' },
+         
+        ]
+      });
+    }else{
+      var camerasCache  = localFactory.get('cameras'); 
+      if(camerasCache){
+        if(angular.isArray(camerasCache)){
+          camerasCache.push($scope.novaCamera);
+          localFactory.set('cameras',camerasCache);
+        }else{
+          localFactory.set('cameras',[$scope.novaCamera]);
+        }
+      }else{
+        localFactory.set('cameras',[$scope.novaCamera]);
+      }
+
+      $scope.adicionar            = false;
+      $scope.novaCamera           = {};
+
+    }
   }
+
+  $scope.editaCamera = function(index){
+    $scope.novaCamera = $scope.cameras[index];
+    $scope.editar     = true;
+    $scope.editarIndice = index;
+  }
+
+  $scope.salvarEditCamera = function(){
+    var camerasCache                  = localFactory.get('cameras'); 
+    camerasCache[$scope.editarIndice] = $scope.novaCamera;
+    
+    localFactory.set('cameras',camerasCache); 
+    
+    $scope.editar               = false;
+    $scope.novaCamera           = {};
+  }
+
+
+  $scope.visualizarCamera = function(){
+       $ionicPopup.show({
+        template: "Deseja visualizar esta camera?",
+        title: 'Atenção',
+        
+        scope: $scope,
+        buttons: [
+         { text: 'Sim',
+           onTap: function(){
+              //CODIGO PARA VISUALIZAR CAMERA RTSP PLUGIN
+           } },{ text: 'Não',
+           onTap: function(){
+            
+           } }
+         
+        ]
+      });
+  }
+
+  $scope.deletarCamera = function(index){
+    $ionicPopup.show({
+        template: "Deseja deletar esta camera?",
+        title: 'Atenção',
+        
+        scope: $scope,
+        buttons: [
+         { text: 'Sim',
+           onTap: function(){
+              var camerasCache                  = localFactory.get('cameras'); 
+              camerasCache.splice($scope.editarIndice,1);
+              localFactory.set('cameras',camerasCache); 
+              
+              $scope.carrageCameras();
+           } },{ text: 'Não',
+           onTap: function(){
+            
+           } }
+         
+        ]
+      });
+    
+  }
+
+
 
 });
